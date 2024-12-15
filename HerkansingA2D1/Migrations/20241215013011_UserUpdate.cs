c0d21cc5b0095d1e6e1c78cc1c8ee70d69885781
C0rpsy <2065665hoof@zuyd.nl>
2024-12-15 02:52:08 +0100

    Absolutely the wrong branch btw, this happened on accident and i honestly dont wanna rebranch.
    Add AppUser entity and update views and layout
    
    Introduce AppUser entity with required fields: Id, UserName, Password, Email, and Role. Update HerkansingA2D1ContextModelSnapshot.cs to include AppUser configuration. Modify Index.cshtml to change title to "Welcome" and replace user info table with a styled layout featuring a welcome message and buttons for "Register" and "Log In". Update _Layout.cshtml to add a new "Account" navigation link. Add migration 20241215013011_UserUpdate.cs to create AppUser table and its designer file.

---
+++ HerkansingA2D1/Migrations/20241215013011_UserUpdate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HerkansingA2D1.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppUser",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppUser", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppUser");
+        }
+    }
+}
